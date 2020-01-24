e7c0d30904bbe7cd5ae192252730390740124a3e
Sterna Angelo Giosue <sterna.angelogiosue@osdbsesto>
2020-01-24 10:01:45 +0100

    add index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ btn.innerHTML = "clicca";
 var body = document.getElementsByTagName("body")[0];
 btn.append(btn);
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function (){
     document.getElementById("h1").innerHTML = "ciao come stai";
 });
